bb710d4bd2fa49c5b4af123db837d2466d4e532c
Masatoshi Teruya <mah0x211@gmail.com>
2015-06-03 19:44:53 +0900

    add: missing file has been added.

---
+++ test/test_http.h
@@ -0,0 +1,14 @@
+#include <unistd.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include <limits.h>
+#include <assert.h>
+#include "../src/http.h"
+
+typedef struct {
+    int rc;
+    uint16_t request;
+    uint8_t nheader;
+    char *entity;
+} test_req_t;
